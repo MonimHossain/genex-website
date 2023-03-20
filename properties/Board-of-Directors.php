e82dad5a3e8834758f33d5918f88de6491549282
Monim Hossain <flynmonim@gmail.com>
2023-03-20 17:37:09 +0600

    board of director

--- properties/Board-of-Directors.php
+++ properties/Board-of-Directors.php
@@ -5,7 +5,7 @@
 </script>
 
 
-<div class="row" style="cursor:pointer;">
+<div class="row" style="cursor:pointer; margin-bottom: 100px">
   <div class="collapse-chevron content" style="padding-bottom: 0px;">
     <h3 data-toggle="collapse" class="collapsed" href="#collapseFilter" style="color: #22bdb6;font-size: 20px;" onclick="changeURL1()">
       <span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span>
@@ -16,7 +16,7 @@
     <div class="collapse" id="collapseFilter">
       <div class="leadership">
         <div class="col-md-12 text-center">
-          <div class="col-md-4 leadershipCard" onclick="openNav(1)">
+          <div class="col-md-4 leadershipCard" onclick="openNav(1)"> 
             <img src="images/team/Chairman sir photo.jpg">
             <h1>Chowdhury Fazle Imam</h1>
             <p>Chairman</p>
@@ -115,7 +115,10 @@
                       </div>
                       <hr>
                       <b></b>
-                      A leader in the BPO & IT Services sector and Co–Founder of Genex. Prince Mojumder heading the business operations of Genex with the vision to become a global company. Genex has now expanded its Service Delivery Center in Myanmar and Malaysia with other parts of the continent in the pipeline. Mr. Mojumder worked for BEXIMCO Limited, Bangladesh Online Limited and has over 17 years of experience in customer services, Technology Solutions, Communications Services, Business Process Outsourcing and People Management.
+                      Mr. Prince Mojumder is the Co-Founder & CEO of Genex Infosys Ltd. has an experience of 20+ years in ICT sector. He is heading Genex with the vision to become a global company, under his leadership Genex grew from a 2 people company to a strong organization of 4000+ employees, and diversified business into different arenas of the ICT Industry. <br><br>
+                      He played a key role in developing the BPO & IT Services Industry in Bangladesh and setting up Global Standards. He was the director of the Executive Committee of Bangladesh Association of Call Center & Outsourcing (BACCO), from 2015 - 2017.<br><br>
+                      Mr. Prince started his ICT Career in 1997 and since then he has led various teams and multiple projects with his Positive Leadership, People Management Skills, Business Intelligence & Governance and experience in Customer Services, Technology Solutions, Communications Services, Business Process Outsourcing and People Management. 
+
 
 
                     </div>
@@ -139,10 +142,51 @@
 
 
 
+          <!--<div class="col-md-4 leadershipCard" onclick="openNav(4)">-->
+          <!--  <img src="images/team/tanveer.jpg">-->
+          <!--  <h1>Tanveer Ali</h1>-->
+          <!--  <p>Independent Director</p>-->
+          <!--</div>-->
+          <!--<div id="4" class="overlay" style="background-color: #fff;">-->
+          <!--  <a href="javascript:void(0)" class="closebtn" onclick="closeNav(4)">&times;</a>-->
+          <!--  <div class="overlay-content">-->
+          <!--    <div class="row">-->
+          <!--      <div class="content">-->
+          <!--        <div class="col-md-12 job-details">-->
+          <!--          <div class="col-md-8">-->
+
+          <!--            <div>-->
+          <!--              <h3>Tanveer Ali</h3>-->
+          <!--              <h5>MBA</h5>-->
+          <!--            </div>-->
+          <!--            <hr>-->
+          <!--            <b></b>-->
+          <!--            Mr. Tanveer Ali is a successful entrepreneur and Venture Capitalist partnering at Olympic Industries, G&R, Inflection Ventures, Avarice Investments-Singapore, Opening Bell-Canada etc. He has joined as a Director at Genex Infosys Limited in 2016.-->
+
+
+          <!--          </div>-->
+          <!--          <div class="col-md-4">-->
+          <!--            <div class="job-summary">-->
+          <!--              <div class="header-job" style="font-size: 16px;">Independent Director</div>-->
+          <!--              <div class="job-s text-center">-->
+
+          <!--                <img src="images/team/tanveer.jpg" alt="">-->
+
+          <!--              </div>-->
+          <!--            </div>-->
+          <!--          </div>-->
+
+          <!--        </div>-->
+          <!--      </div>-->
+          <!--    </div>-->
+          <!--  </div>-->
+          <!--</div>-->
+          
+          
           <div class="col-md-4 leadershipCard" onclick="openNav(4)">
-            <img src="images/team/tanveer.jpg">
-            <h1>Tanveer Ali</h1>
-            <p>Independent Director</p>
+            <img src="images/team/Nilfoar Imam.jpg">
+            <h1> Mrs. Nilofar Imam</h1>
+            <p>Director</p>
           </div>
           <div id="4" class="overlay" style="background-color: #fff;">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav(4)">&times;</a>
@@ -153,51 +197,12 @@
                     <div class="col-md-8">
 
                       <div>
-                        <h3>Tanveer Ali</h3>
-                        <h5>MBA</h5>
+                        <h3> Mrs. Nilofar Imam</h3>
+                        <!--<h5>MBA</h5>-->
                       </div>
                       <hr>
-                      <b></b>
-                      Mr. Tanveer Ali is a successful entrepreneur and Venture Capitalist partnering at Olympic Industries, G&R, Inflection Ventures, Avarice Investments-Singapore, Opening Bell-Canada etc. He has joined as a Director at Genex Infosys Limited in 2016.
-
-
-                    </div>
-                    <div class="col-md-4">
-                      <div class="job-summary">
-                        <div class="header-job" style="font-size: 16px;">Independent Director</div>
-                        <div class="job-s text-center">
-
-                          <img src="images/team/tanveer.jpg" alt="">
-
-                        </div>
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 leadershipCard" onclick="openNav(5)">
-            <img src="images/team/ZIC P.jpg">
-            <h1>Mohammad Ziaul Islam Chowdhury</h1>
-            <p>Director</p>
-          </div>
-          <div id="5" class="overlay" style="background-color: #fff;">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav(5)">&times;</a>
-            <div class="overlay-content">
-              <div class="row">
-                <div class="content">
-                  <div class="col-md-12 job-details">
-                    <div class="col-md-8">
-
-                      <div>
-                        <h3>Mohammad Ziaul Islam Chowdhury</h3>
-                        <h5>B.A (Economics), MBA (IBA, DU)</h5>
-                      </div>
-                      <hr>
-                      <b></b>
-                      A seasoned professional with over 20 years of experience in Strategic Planning, Investment and Business Operations and Regulatory Compliance. He was the Managing Partner of Knit Asia Ltd, Managing Director of SQ Clothing and GM of Square Textiles. He is a graduate from Department of Economics, University of Dhaka and MBA from Institute of Business Administration (IBA), University of Dhaka.
+                      <!--<b></b>-->
+                      <!--Mr. Tanveer Ali is a successful entrepreneur and Venture Capitalist partnering at Olympic Industries, G&R, Inflection Ventures, Avarice Investments-Singapore, Opening Bell-Canada etc. He has joined as a Director at Genex Infosys Limited in 2016.-->
 
 
                     </div>
@@ -206,7 +211,7 @@
                         <div class="header-job" style="font-size: 16px;">Director</div>
                         <div class="job-s text-center">
 
-                          <img src="images/team/ZIC P.jpg" alt="">
+                          <img src="images/team/Nilfoar Imam.jpg" alt="">
 
                         </div>
                       </div>
@@ -217,10 +222,11 @@
               </div>
             </div>
           </div>
+          
           <div class="col-md-4 leadershipCard" onclick="openNav(6)">
-            <img src="images/team/Photo for Mr.Mezbah Uddin.jpg">
+            <img src="images/team/mejba-oracle.jpeg">
             <h1>Mr. Mezbah Uddin</h1>
-            <p>Representative Director of Oracle Service Ltd</p>
+            <p>Nominated Director of Oracle Service Ltd</p>
           </div>
           <div id="6" class="overlay" style="background-color: #fff;">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav(6)">&times;</a>
@@ -242,10 +248,10 @@
                     </div>
                     <div class="col-md-4">
                       <div class="job-summary">
-                        <div class="header-job" style="font-size: 16px;">Representative Director of Oracle Service Ltd</div>
+                        <div class="header-job" style="font-size: 16px;">Nominated Director of Oracle Services Ltd</div>
                         <div class="job-s text-center">
 
-                          <img src="images/team/Photo for Mr.Mezbah Uddin.jpg" alt="">
+                          <img src="images/team/mejba-oracle.jpeg" alt="">
 
                         </div>
                       </div>
@@ -256,14 +262,53 @@
               </div>
             </div>
           </div>
-          
-          <div class="col-md-4 leadershipCard" onclick="openNav(66)">
+          <div class="col-md-4 leadershipCard" onclick="openNav(5)">
+            <img src="images/team/ri_genex.jpeg">
+            <h1>Mrs. Rokeya Islam</h1>
+            <p>Independent Director</p>
+          </div>
+          <div id="5" class="overlay" style="background-color: #fff;">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav(5)">&times;</a>
+            <div class="overlay-content">
+              <div class="row">
+                <div class="content">
+                  <div class="col-md-12 job-details">
+                    <div class="col-md-8">
+
+                      <div>
+                        <h3>Mrs. Rokeya Islam</h3>
+                        <!-- <h5>B.A (Economics), MBA (IBA, DU)</h5> -->
+                      </div>
+                      <hr>
+                      <b></b>
+                      Mrs. Rokeya Islam is a Post-Graduate from the University of Dhaka. She has extensive experience in customer service and management having worked in Standard Chartered Bank and Premier Banks for 10 years. After completing her tenures with Banks, she has embarked on a successful career as a culinary, fashion and interior design work in each service sector. She is a widely traveled person, and has consistence and relationship experience in the private sector to allow her to a leadership role on the Board of the Company.
+
+
+                    </div>
+                    <div class="col-md-4">
+                      <div class="job-summary">
+                        <div class="header-job" style="font-size: 16px;">Independent Director</div>
+                        <div class="job-s text-center">
+
+                          <img src="images/team/ri_genex.jpeg" alt="">
+
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 leadershipCard" onclick="openNav(555)">
             <img src="images/team/Md. Nazmul Hassan.jpg">
             <h1>MR. MD Nazmul Hasan</h1>
             <p>Independent Director</p>
           </div>
-          <div id="6" class="overlay" style="background-color: #fff;">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav(66)">&times;</a>
+          <div id="555" class="overlay" style="background-color: #fff;">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav(555)">&times;</a>
             <div class="overlay-content">
               <div class="row">
                 <div class="content">
@@ -272,7 +317,7 @@
 
                       <div>
                         <h3>MR. MD Nazmul Hasan</h3>
-                        <h5>ICMAB</h5>
+                        <!-- <h5>B.A (Economics), MBA (IBA, DU)</h5> -->
                       </div>
                       <hr>
                       <b></b>
@@ -302,13 +347,12 @@
               </div>
             </div>
           </div>
-
           <div class="col-md-4 leadershipCard" onclick="openNav(20221)">
-            <img src="images/team/Md. Nazmul Hassan.jpg">
+            <img src="images/team/nuruladvisor.jpg">
             <h1>Mr. T I M Nurul Kabir</h1>
-            <p>Advisor to the Board</p>
+            <p>Independent Director</p>
           </div>
-          <div id="6" class="overlay" style="background-color: #fff;">
+          <div id="20221" class="overlay" style="background-color: #fff;">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav(20221)">&times;</a>
             <div class="overlay-content">
               <div class="row">
@@ -318,7 +362,7 @@
 
                       <div>
                         <h3>Mr. T I M Nurul Kabir</h3>
-                        <h5>Advisor to the Board</h5>
+                        <h5>Independent Director</h5>
                       </div>
                       <hr>
                       <b></b>
@@ -344,6 +388,7 @@
               </div>
             </div>
           </div>
+
 
 
         </div>
