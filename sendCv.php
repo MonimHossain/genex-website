45e1c561e9e87d6af6448554193990d7e478c934
Monim Hossain <flynmonim@gmail.com>
2023-05-30 19:00:14 +0600

    minor

--- sendCv.php
+++ sendCv.php
@@ -190,7 +190,8 @@ if(isset($_POST["submit"])){
 
     } catch (Exception $e) {
 
-        //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        echo "Message could not be sent. Mailer Error: {$e}";
+        exit;
 
         echo"<script>window.location.replace('applynow.php?successMsg=11')</script>";
 
